70bfe44e35011fba368bf8b51a8099752661e96b
Daniil Dmitriev <ocksumoron@yandex.ru>
2016-09-08 20:24:16 +0300

    Initial Commit

---
+++ game_of_life/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game_of_life
+//
+//  Created by Даня on 08.09.16.
+//  Copyright © 2016 mipt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
